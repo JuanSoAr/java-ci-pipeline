cee3e3366593969cf2a3a20b41c777132d3bc932
JuanSoAr <jsolisar12@gmail.com>
2020-05-22 10:42:34 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+    int suma(int a,int b){
+        return a+b;
+    }
+    int resta(int a,int b){
+        return a-b;
+    }
+    int multiplicacion(int a,int b){
+        return a*b;
+    }
+    int divicion(int a,int b){
+        return a/b;
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
